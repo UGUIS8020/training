09a1f4443d2f99108bc7ab610fb735a2ecc6197a
UGUIS8020 <shibuyamasahiko@gmail.com>
2025-02-21 23:16:34 +0900

    新越谷

---
+++ chat_response.py
@@ -0,0 +1,16 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+llm = ChatOpenAI(model_name="gpt-4", temperature=0, openai_api_key=openai_api_key)
+
+messages = [HumanMessage(content="自己紹介してください。")]
+print(messages)
+
+result = llm.invoke(messages)
+print(result.content)
